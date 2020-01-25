66030c80922f04a405ef982e254885e2a4e5d877
serg0 <serg.01@bk.ru>
2020-01-25 18:34:50 +0300

    base adapter

--- app/src/main/java/com/example/longclick/MainActivity.java
+++ app/src/main/java/com/example/longclick/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.longclick;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
@@ -17,42 +20,36 @@ public class MainActivity extends AppCompatActivity {
     Button button;
     ArrayList<HashMap<String, String>> arrayList = new ArrayList<>();
     HashMap<String, String> map;
-
+    BaseAdapter listContentAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         listView = findViewById(R.id.zone_list);
 
-        String Name = "Name";
-        String Description = "Description";
-        Data firstObject = new Data("Anton","Funny kid",12);
-        Data secondObject = new Data("Georg","Ded",33);
-
-
+        Data firstObject = new Data(getString(R.string.anton),getString(R.string.anton_description),12);
+        Data secondObject = new Data(getString(R.string.georg),getString(R.string.georg_description),33);
         map = new HashMap<>();
-        map.put(Name,firstObject.getName());
-        map.put(Description,firstObject.getDescription());
+        map.put(firstObject.getName(),firstObject.getDescription());
+        map.put(secondObject.getName(),secondObject.getDescription());
         arrayList.add(map);
 
-        map = new HashMap<>();
-        map.put(Name,secondObject.getName());
-        map.put(Description,secondObject.getDescription());
-        arrayList.add(map);
-
-        final SimpleAdapter adapter = new SimpleAdapter(this ,arrayList , R.layout.item_list_view ,
-                new String[]{Name,Description},
-                new int[]{R.id.Name,R.id.Description});
-        listView.setAdapter(adapter);
+        listContentAdapter = createAdapter(arrayList);
+        listView.setAdapter(listContentAdapter);
 
         button = findViewById(R.id.checkBox);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-               Object number = adapter.getItem(position);
-               arrayList.remove(number);
-               adapter.notifyDataSetChanged();
+               arrayList.remove(position);
+               listContentAdapter.notifyDataSetChanged();
+               listView.setAdapter(listContentAdapter);
             }
         });
     }
+    @NonNull
+    private BaseAdapter createAdapter(ArrayList arrayList) {
+        return new ArrayAdapter<>(this, R.layout.item_list_view, arrayList);
+    }
+
 }
